109b8707cd01b5b50e372807ff48ebc01498a3c5
Micael-Martins <134942759+Micael-Martins@users.noreply.github.com>
2023-09-19 13:33:49 -0300

    Create q5.c

---
+++ q5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int v1, v2;
+    
+    scanf("%d", &v1);
+    scanf("%d", &v2);
+
+    if(v1 == v2){
+        printf("Os valores lidos sao iguais");
+    }
+    else if(v1 > v2){
+        printf("%d eh maior que %d\n", v1, v2);
+        if( v1 % v2 == 0){
+            printf("%d eh multiplo de %d", v1, v2);
+        }
+        else {
+            printf("%d nao eh multiplo de %d", v1, v2);
+        }
+    }
+    else if(v1 < v2){
+        printf("%d eh maior que %d\n", v2, v1);
+        if( v2 % v1 == 0){
+            printf("%d eh multiplo de %d", v2, v1);
+        }
+        else {
+            printf("%d nao eh multiplo de %d", v2, v1);
+        }
+    }
+    return 0;
+}
