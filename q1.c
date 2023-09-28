a9dde8b63a02f3b9faeea7370ba46203049d3f55
Micael-Martins <134942759+Micael-Martins@users.noreply.github.com>
2023-09-28 11:10:20 -0300

    Update q1.c
    Agora funciona.

--- q1.c
+++ q1.c
@@ -8,36 +8,31 @@ int main(){
 
      scanf("%d", &AnoEscolhido);
 
-    for ( inicial = 1896; inicial <= 2022; jogos = inicial = inicial + 4){
+    for ( inicial = 1892; inicial <= 2022; jogos = inicial = inicial + 4){
         if (AnoEscolhido < 1896)
         {
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d", AnoEscolhido);
-        break;
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", AnoEscolhido);
+        return 0;
         }
         else if (AnoEscolhido == 1916 || AnoEscolhido == 1940 || AnoEscolhido == 1942 || AnoEscolhido == 1944 || AnoEscolhido == 1946){
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d", AnoEscolhido);
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", AnoEscolhido);
         return 0;
-        break;
-        }
-        else if (jogos != AnoEscolhido){
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d", AnoEscolhido);
-        break;
         }
         else if (jogos == AnoEscolhido){
-        printf("Os jogos Olimpicos de Verao ocorreram no ano de %d", AnoEscolhido);
-        break;
+        printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.", AnoEscolhido);
+        return 0;
         }
-        
+
     }
-    for ( inicial = 1930; inicial <= 2022; copa = inicial = inicial + 4)
+    for ( inicial = 1926; inicial <= 2022; copa = inicial = inicial + 4)
     {
        if (copa == AnoEscolhido)
        {
-        printf("A copa do mundo ocorreu no ano de %d", AnoEscolhido);
-        break;
+        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.", AnoEscolhido);
+        return 0;
        }
-       
-    }
 
+    }
+    printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", AnoEscolhido);
 return 0;    
 }
