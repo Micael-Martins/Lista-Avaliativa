f8ebc2f109f724d644368d25400da0f19471e846
Micael-Martins <134942759+Micael-Martins@users.noreply.github.com>
2023-09-28 11:24:52 -0300

    Update q5.c
    Funcionando

--- q5.c
+++ q5.c
@@ -9,9 +9,19 @@ int main() {
     if(v1 == v2){
         printf("Os valores lidos sao iguais");
     }
+    else if(v1 == 0 || v2 == 0){
+        if(v1 > v2){
+            printf("%d eh maior que %d\n", v1, v2);
+            printf("%d nao eh multiplo de %d", v1, v2);
+        }
+        else{
+            printf("%d eh maior que %d\n", v2, v1);
+             printf("%d nao eh multiplo de %d", v2, v1);
+        }
+    }
     else if(v1 > v2){
         printf("%d eh maior que %d\n", v1, v2);
-        if( v1 % v2 == 0){
+        if( (v1 != 0) && v1 % v2 == 0){
             printf("%d eh multiplo de %d", v1, v2);
         }
         else {
@@ -20,7 +30,7 @@ int main() {
     }
     else if(v1 < v2){
         printf("%d eh maior que %d\n", v2, v1);
-        if( v2 % v1 == 0){
+        if( (v2 != 0) && v2 % v1 == 0){
             printf("%d eh multiplo de %d", v2, v1);
         }
         else {
