673c67957ba08713b6f7c4d2fa5039a0070018b1
Micael-Martins <134942759+Micael-Martins@users.noreply.github.com>
2023-09-28 11:17:22 -0300

    Update q5.c

--- q5.c
+++ q5.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 
-int main(){
-    int inicial;
-    int AnoEscolhido;
-    int copa;
-    int jogos;
+int main() {
+    int v1, v2;
 
-     scanf("%d", &AnoEscolhido);
+    scanf("%d", &v1);
+    scanf("%d", &v2);
 
-    for ( inicial = 1892; inicial <= 2022; jogos = inicial = inicial + 4){
-        if (AnoEscolhido < 1896)
-        {
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", AnoEscolhido);
-        return 0;
-        }
-        else if (AnoEscolhido == 1916 || AnoEscolhido == 1940 || AnoEscolhido == 1942 || AnoEscolhido == 1944 || AnoEscolhido == 1946){
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", AnoEscolhido);
-        return 0;
-        }
-        else if (jogos == AnoEscolhido){
-        printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.", AnoEscolhido);
-        return 0;
-        }
-
+    if(v1 == v2){
+        printf("Os valores lidos sao iguais");
     }
-    for ( inicial = 1926; inicial <= 2022; copa = inicial = inicial + 4)
-    {
-       if (copa == AnoEscolhido)
-       {
-        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.", AnoEscolhido);
-        return 0;
-       }
-
+    else if(v1 > v2){
+        printf("%d eh maior que %d\n", v1, v2);
+        if( v1 % v2 == 0){
+            printf("%d eh multiplo de %d", v1, v2);
+        }
+        else {
+            printf("%d nao eh multiplo de %d", v1, v2);
+        }
     }
-    printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", AnoEscolhido);
-return 0;    
+    else if(v1 < v2){
+        printf("%d eh maior que %d\n", v2, v1);
+        if( v2 % v1 == 0){
+            printf("%d eh multiplo de %d", v2, v1);
+        }
+        else {
+            printf("%d nao eh multiplo de %d", v2, v1);
+        }
+    }
+    return 0;
 }
